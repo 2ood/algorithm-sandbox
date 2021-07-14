5316a8d400a4ed5cedf5b93812aa76c15077f189
2ood <tommychoi0128@gmail.com>
2021-07-14 14:52:02 +0000

    fuck baekjoon

--- Main.java
+++ Main.java
@@ -3,45 +3,38 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.StringTokenizer;
+import java.util.Arrays;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
-	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));	
-	StringTokenizer tk = new StringTokenizer(br.readLine());	
-	int len = Integer.parseInt(tk.nextToken());
-    StringBuilder sb = new StringBuilder();
-    Arraylist nums[] = new ArrayList[len];
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));	
+        StringTokenizer tk = new StringTokenizer(br.readLine());
         
-    for(int i=0;i<nums.length;i++) {
-        tk = new StringTokenizer(br.readLine());
-        while(tk.hasMoreTokens()) {
-            nums[i].add(new Integer(tk.nextToken()));
-        }
-        nums[i].trimToSize();
-    }
-    
-    for(int i=0;i<nums.length;i++) {
-        sb.append(gcdSum(nums[i])).append("\n");
-    }
+        int n = Integer.parseInt(tk.nextToken());
+        int m = Integer.parseInt(tk.nextToken());
         
-    System.out.print(sb.toString());
+        k = new int[n][m];
         
-    }
-    
-    public static int gcd (int mx, int mn) {
-        if(mn>=1) return gcd(mn, mx%mn);
-        else return mx;
-    }
-    
-    public static int gcdSum(ArrayList<Integer> list) {
-        int result=0;
-        for(int i=0; i<list.size();i++) {
-            for(int j=i; j<list.size();j++) {
-                result+=gcd(Math.max(list.get(i),list.get(j)),Math.min(list.get(i),list.get(j)));
+        for(int i=0;i<n;i++) {
+            tk = new StringTokenizer(br.readLine());
+            for(int j=0;j<m;j++) {
+                k[i][j]=Integer.parseInt(tk.nextToken());
             }
         }
         
-        return result;
+        int maxSum=0;
+        int tmpSum=0;
+        
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<m;j++) {
+
+               
+            }
+        }
+        
+        
     }
+    
+    
 }
