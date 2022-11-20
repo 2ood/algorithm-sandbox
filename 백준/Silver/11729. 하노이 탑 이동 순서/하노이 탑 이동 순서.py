f6c6c72a53db75267e0ccf15bffe606bc5e9e306
Kyungmin Choi <kyungmin.official0@gmail.com>
2022-11-20 22:44:32 +0900

    [Silver I] Title: 하노이 탑 이동 순서, Time: 1236 ms, Memory: 105648 KB -BaekjoonHub

---
+++ 백준/Silver/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.py
@@ -0,0 +1,22 @@
+
+def solution(li, fr, temp, to, num) :
+    # print("moving from"+str(fr)+" to "+str(to)+"with amount"+str(num))
+
+    if(num<=1) :
+        li.append(move(fr,to))
+        return 1
+    else :
+        t1 = solution(li, fr, to, temp, num-1)
+        li.append(move(fr,to))
+        t2 = solution(li, temp,fr,to, num-1)
+        return t1+1+t2
+
+
+def move(fr, to) :
+    return str(fr)+" "+str(to)
+
+progress = list()
+print(solution(progress, 1,2,3,int(input())))
+
+for i in range(len(progress)): 
+    print(progress[i])
