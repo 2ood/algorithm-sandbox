c4dc992b50a790a318f92979c04e4f0a8d42bc00
Kyungmin Choi <kyungmin.official0@gmail.com>
2025-03-25 23:12:30 +0900

    [Gold V] Title: 하노이 탑 이동 순서, Time: 876 ms, Memory: 90836 KB -BaekjoonHub

--- 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.py
+++ 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.py
@@ -1,21 +1,30 @@
-def solution(li, fr, temp, to, num) :
-    # print("moving from"+str(fr)+" to "+str(to)+"with amount"+str(num))
+import sys
 
-    if(num<=1) :
-        li.append(move(fr,to))
-        return 1
+def not_selected_col(col1,col2):
+    if col1 ==1 :
+        return 2 if col2==3 else 3
+    elif col1 ==2 :
+        return 1 if col2==3 else 3
     else :
-        t1 = solution(li, fr, to, temp, num-1)
-        li.append(move(fr,to))
-        t2 = solution(li, temp,fr,to, num-1)
-        return t1+1+t2
+        return 1 if col2==2 else 2
 
 
-def move(fr, to) :
-    return str(fr)+" "+str(to)
+def hanoi(move_size,move_from_col,move_to_col,moves):
+    if move_size <=1 :
+        moves.append(" ".join([str(move_from_col),str(move_to_col)]))
+    
+    else :
+        temp_col = not_selected_col(move_from_col,move_to_col)
+        hanoi(move_size-1,move_from_col,temp_col,moves)
+        moves.append(" ".join([str(move_from_col),str(move_to_col)]))
+        hanoi(move_size-1,temp_col,move_to_col,moves)
 
-progress = list()
-print(solution(progress, 1,2,3,int(input())))
 
-for i in range(len(progress)): 
-    print(progress[i])
+n = int(sys.stdin.readline())
+moves = []
+hanoi(n,1,3,moves)
+
+print(len(moves))
+
+for move in moves :
+    print(move)
