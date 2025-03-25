1d9c2bb78d05e049a4b65b57ff62c16a3884bc0c
2ood <kyungmin.official0@gmail.com>
2025-03-25 22:46:12 +0900

    save

--- test.py
+++ test.py
@@ -1,18 +1,44 @@
-def cho(n, m):
-    c = 0
+test = [
+    (["5 21"],"26"),
+    (["10 500"],"510")
+]
 
-    if(n == 1 or m == 1):
-        c = m+n-2
-    else:
-        mx = n if n > m else m
-        mn = m if n > m else n
-        c = cho(mn//2, mx)+cho(mn//2+mn % 2, mx)+1
+def max_sum(candidates, currently, target, cards) :
+    if len(candidates) == 0 :
+        return currently
+    elif currently == target :
+        return currently
+    elif cards == 0 :
+        return currently
+    else :
+        number = candidates.pop(0)
+        without_this = max_sum(candidates,currently,target,cards)
+        if currently+number > target :
+            return without_this
+        else :
+            with_this = max_sum(candidates,currently+number,target,cards-1)
+            return max(with_this, without_this)
 
-    return c
+def solve(input) :
+    size, maximum = map(int,input[0].split())
+    candidates = list(map(int,input[1].split()))
+    
+    return str(max_sum(candidates,0, maximum,3))
+             
+            
+            
+            
 
 
-d = input().split()
-a = int(d[0])
-b = int(d[1])
 
-print(cho(a, b))
+for i in range(len(test)) :
+    solv = solve(test[i][0])
+    print("input : "+str(test[i][0]))
+    print("Expected : "+str(test[i][1]))
+    print("Output : "+str(solv))
+    
+    try : 
+        assert solv == test[i][1]
+        print("test case "+str(i)+" passed")
+    except :
+        print("Error in test case "+str(i))
