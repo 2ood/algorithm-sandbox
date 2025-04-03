232addccd409af6f5eeb79071ce152fa2d43c2b5
2ood <kyungmin.official0@gmail.com>
2025-04-03 17:16:36 +0900

    wrotesomthing

--- cli_test.py
+++ cli_test.py
@@ -1,23 +1,59 @@
 import sys
 
-def binary_search(numberlist, target, start, end) :
-    if(start >= end) :
-        return 1 if numberlist[start] ==target else 0
+class Node :
+    def __init__(self, content=1):
+        self.next = None
+        self.content = content
     
-    new_index = (start+end)//2
-    if target == numberlist[new_index] :
-        return 1
-    elif target < numberlist[new_index] :
-        return binary_search(numberlist, target, new_index,end)
-    else :
-        return binary_search(numberlist, target, start, new_index)
+    def __str__(self) :
+        return 
+    
+class CircularLinkedList :
+    def __init__(self):
+        self.pointed = Node()
+        self.pointed.next = self.pointed
+        self.length = 1
+        
+    def append(self, node):
+        node.next = self.pointed.next
+        self.pointed.next = node
+        self.length +=1
+    
+    def pop(self, k=1):
+        if self.length <= 0 : # empty
+            raise Exception("the list is empty")
 
-n = int(sys.stdin.readline())
-numberlist = list(map(int,sys.stdin.readline().split()))
-numberlist.sort()
+        elif self.length ==1 :
+            popped_node = self.pointed
+            self.pointed = None
+            self.length =0
+            return popped_node
+        
+        else :
+            for i in range(k//self.length-1) :
+                self.pointed = self.pointed.next
+            popped_node = self.pointed.next
+            self.pointed.next = popped_node.next
+            self.length-=1
+            return popped_node
+    
+    def __str__(self) :
+        result_string = "<"
+        while self.length !=0 :
+            result_string+=str(self.pop())
+            if self.length > 0 :
+                result_string+=", "
+            else :
+                retuls_string+=">"
+        
+        return result_string
+        
 
-m = int(sys.stdin.readline())
-targets = list(map(int,sys.stdin.readline().split()))
 
-for i in range(int(m)) :
-    print(binary_search(numberlist,targets[i],0,len(numberlist)))
+[n,m] = list(map(int,sys.stdin.readline().split()))
+
+c_list = CircularLinkedList()
+for i in range(2,n) :
+    c_list.append(Node(i))
+
+print(c_list)
